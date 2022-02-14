ecfb2fc4a86b1f581e74a561baca90e2db2f877d
anniejoel <annhayriyan@gmail.com>
2022-02-14 22:15:31 +0400

    finished

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
 let submit = document.getElementById("submit");
 let clear = document.getElementById("clear");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let interval = document.getElementById("interval");
+
 document.getElementById("add").addEventListener("click", ()=> {
     quantity.value++;
 })
@@ -21,9 +25,26 @@ function add(a) {
     return (b) => a+b;
 }
 
-clear.addEventListener("click", ()=> {
+clear.addEventListener("click", () => {
         answer.value = "";
         num1.value = "";
         num2.value = "";
     }
 )
+
+let intro;
+
+function startTimeout() {
+      intro = setInterval(() => {
+         interval.value++;
+     }, 1000)
+}
+
+function stopInterval(){
+    clearInterval(intro);
+    interval.value="";
+}
+
+
+start.addEventListener("click", startTimeout);
+stop.addEventListener("click", stopInterval);
